d67d2e17b20321ee474c1af76280bfca6b2f5170
Michael Wells <mwells1286@gmail.com>
2020-01-17 11:00:43 -0800

    add method to titleize products before save. passes rspec.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  before_save(:titleize_product)
   has_many :reviews, dependent: :destroy
   validates :name, :cost, :country_of_origin, presence: true
+  private
+    def titleize_product
+      self.name = self.name.titleize
+    end
 end
