408fdd3523ec76ef8a2a520c27648be531d64c79
Michael Wells <mwells1286@gmail.com>
2020-01-17 10:28:16 -0800

    add test for validating presence of inputs for products.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Product do
+
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+  it { should validate_presence_of :country_of_origin }
   it {should have_many(:reviews)}
 context '#destroy' do
   it 'should delete product' do
