7387104c198f830dc30014ab004e9d87d2c68bf5
Michael Wells <mwells1286@gmail.com>
2020-01-17 08:33:44 -0800

    add db migration and spec files

---
+++ db/migrate/20200117161544_create_foods_and_reviews.rb
@@ -0,0 +1,18 @@
+class CreateFoodsAndReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.column(:name, :string)
+      t.column(:cost, :decimal)
+      t.column(:country_of_origin, :string)
+
+      t.timestamps()
+    end
+    create_table :reviews do |t|
+      t.column(:author, :string)
+      t.column(:content_body, :string)
+      t.column(:rating, :integer)
+
+      t.timestamps()
+    end
+  end
+end
