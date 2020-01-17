87368e9c435cf23d9dce03c4c3e3d8e491088196
Michael Wells <mwells1286@gmail.com>
2020-01-17 09:46:17 -0800

    add test for delete functionality

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,3 +3,10 @@ require 'rails_helper'
 describe Product do
   it {should have_many(:reviews)}
 end
+context 'DELETE #destroy' do
+  product = Product.create(name: "pie", cost: "33.33", country_of_origin: "usa")
+
+  it 'should delete product' do
+    expect { product.destroy }.to change(Product, :count).by(-1)
+  end
+end
