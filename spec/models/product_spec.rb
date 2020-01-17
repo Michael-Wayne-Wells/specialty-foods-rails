ec40a9300323a567d609f5c6ff7d128fb8b9c93b
Michael Wells <mwells1286@gmail.com>
2020-01-17 09:59:05 -0800

    add spec for review crud functionality

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 describe Product do
   it {should have_many(:reviews)}
-end
 context '#destroy' do
   it 'should delete product' do
     product = Product.create(name: "pie", cost: "33.33", country_of_origin: "usa")
     expect { product.destroy }.to change(Product, :count).by(-1)
   end
+end
   context '#create' do
     it 'should create a new product' do
       product = Product.create(name: "pie", cost: "33.33", country_of_origin: "usa")
       expect(Product.all).to include(product)
     end
   end
-    context '#update' do
-      it 'should update product' do
-        product = Product.create(name: "pie", cost: "33.33", country_of_origin: "usa")
-        product.update(name: "cheese", cost: "33.33", country_of_origin: "usa")
-        expect(product.name).to eq("cheese")
+  context '#update' do
+    it 'should update product' do
+      product = Product.create(name: "pie", cost: "33.33", country_of_origin: "usa")
+      product.update(name: "cheese", cost: "33.33", country_of_origin: "usa")
+      expect(product.name).to eq("cheese")
+    end
   end
-end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 describe Review do
   it {should belong_to(:product)}
+context '#destroy' do
+  it 'should delete review' do
+    review = Review.create(author: "steve", content_body: "wow!", rating: 5)
+    expect { review.destroy }.to change(Review, :count).by(-1)
+  end
+end
+  context '#create' do
+    it 'should create a new review' do
+      review = Review.create(author: "steve", content_body: "wow!", rating: 5)
+      expect(Review.all).to include(review)
+    end
+  end
+  context '#update' do
+    it 'should update review' do
+      review = Review.create(author: "steve", content_body: "wow!", rating: 5)
+      review.update(author: "jim", content_body: "wow!", rating: 5)
+      expect(review.author).to eq("jim")
+    end
+  end
 end
