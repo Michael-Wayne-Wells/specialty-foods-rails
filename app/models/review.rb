c75f5380937bbd737bf9255bc8e34be66a99a629
Michael Wells <mwells1286@gmail.com>
2020-01-17 10:45:30 -0800

    add validations for content and rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true
+  validates :content_body, length: { maximum: 250, minumum: 50 }, presence: true
+  validates :rating,  numericality: { greater_than: 0, less_than_or_equal_to: 5 }, presence: true
 end
