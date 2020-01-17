d1430fb932c7c0037480bda28e1ccb00552d7135
Michael Wells <mwells1286@gmail.com>
2020-01-17 11:54:53 -0800

    add most viewed scope.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
+
+  scope :most_reviewed, -> {(
+    select("products.*, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
   scope :usa, -> { where(country_of_origin: "Usa")}
   scope :recent_products, -> { order(created_at: :desc).limit(3)}
   before_save(:titleize_product, :titleize_country)
