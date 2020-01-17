a1d02f9dcea61a8886931c66c820c178fff29eb7
Michael Wells <mwells1286@gmail.com>
2020-01-17 11:05:54 -0800

    add scope for products made in the usa

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ApplicationRecord
-  before_save(:titleize_product)
+  scope :usa, -> { where(country_of_origin: "Usa")}
+  before_save(:titleize_product, :titleize_country)
   has_many :reviews, dependent: :destroy
   validates :name, :cost, :country_of_origin, presence: true
   private
     def titleize_product
       self.name = self.name.titleize
+    end
+    def titleize_country
+      self.country_of_origin = self.country_of_origin.titleize
     end
 end
