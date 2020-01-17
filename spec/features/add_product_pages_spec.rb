46fd823ca9a248f02375c07854603556ebd8b8fe
Michael Wells <mwells1286@gmail.com>
2020-01-17 15:57:17 -0800

    add integration test for creating a review

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "the add a product process" do
+describe "the add,delete, and edit a product process" do
   it "adds a new product" do
     visit products_path
     click_link 'New Product'
@@ -30,7 +30,7 @@ describe "the add a product process" do
     click_on 'Submit'
     expect(page).to have_content "Hair"
   end
-  it "updates product" do
+  it "deletes product" do
     visit products_path
     click_link 'New Product'
     fill_in 'product[name]', :with => 'teeth'
@@ -41,4 +41,20 @@ describe "the add a product process" do
     click_on 'Delete'
     expect(page).not_to have_content "Teeth"
   end
+  it "adds a new product" do
+    visit products_path
+    click_link 'New Product'
+    fill_in 'product[name]', :with => 'teeth'
+    fill_in 'product[cost]', :with => '32.27'
+    fill_in 'product[country_of_origin]', :with => "turkey"
+    click_on 'Submit'
+    click_on 'Teeth'
+    click_on 'Add a review'
+    fill_in 'review[author]', :with => 'Jim'
+    fill_in 'review[content_body]', :with => 'wow'
+    fill_in 'review[rating]', :with => '3'
+    click_on 'Create Review'
+    expect(page).to have_content "Jim"
+  end
+
 end
