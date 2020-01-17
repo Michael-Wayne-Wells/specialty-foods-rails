46756128b282028e889fe4307dac97189a831b91
Michael Wells <mwells1286@gmail.com>
2020-01-17 10:11:09 -0800

    add methods for create, update, and destroy that pass rspec.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,21 +2,24 @@ require 'rails_helper'
 
 describe Review do
   it {should belong_to(:product)}
-context '#destroy' do
-  it 'should delete review' do
-    review = Review.create(author: "steve", content_body: "wow!", rating: 5)
-    expect { review.destroy }.to change(Review, :count).by(-1)
+  context '#destroy' do
+    it 'should delete review' do
+      product = Product.create(name: "pie", cost: "33.33", country_of_origin: "usa")
+      review = product.reviews.create(author: "steve", content_body: "wow!", rating: 5)
+      expect { review.destroy }.to change(Review, :count).by(-1)
+    end
   end
-end
   context '#create' do
     it 'should create a new review' do
-      review = Review.create(author: "steve", content_body: "wow!", rating: 5)
-      expect(Review.all).to include(review)
+      product = Product.create(name: "pie", cost: "33.33", country_of_origin: "usa")
+      review = product.reviews.create(author: "steve", content_body: "wow!", rating: 5)
+      expect(product.reviews.all).to include(review)
     end
   end
   context '#update' do
     it 'should update review' do
-      review = Review.create(author: "steve", content_body: "wow!", rating: 5)
+      product = Product.create(name: "pie", cost: "33.33", country_of_origin: "usa")
+      review = product.reviews.create(author: "steve", content_body: "wow!", rating: 5)
       review.update(author: "jim", content_body: "wow!", rating: 5)
       expect(review.author).to eq("jim")
     end
