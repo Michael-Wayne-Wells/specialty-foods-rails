dbc115c76d29b130e4bf4a9d9cbaf444ca585f81
Michael Wells <mwells1286@gmail.com>
2020-01-17 10:45:13 -0800

    add tests to check for presence and min/max of content and rating for reviews.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe Product do
-
+context "validations" do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
   it {should have_many(:reviews)}
+end
 context '#destroy' do
   it 'should delete product' do
     product = Product.create(name: "pie", cost: "33.33", country_of_origin: "usa")
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe Review do
   context "validations" do
     it { should validate_presence_of :author }
+    it { should validate_presence_of :content_body }
+    it { should validate_length_of :content_body }
+    it { should validate_presence_of :rating }
+    it { should validate_numericality_of :rating }
+
+
   end
 
   it {should belong_to(:product)}
