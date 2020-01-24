c45dc0acd83822a6f7262d552109ccea64121650
Michael Wells <mwells1286@gmail.com>
2020-01-24 15:41:23 -0800

    add reviews admin console

---
+++ app/admin/reviews.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register Review do
+  permit_params :author, :content_body, :rating
+
+  index do
+    selectable_column
+    id_column
+    column :author
+    column :content_body
+    column :rating
+
+    actions
+  end
+end
