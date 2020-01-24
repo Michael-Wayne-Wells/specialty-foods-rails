616868d0e6ccc34411a3ae0b472f47133fe6b2f6
Michael Wells <mwells1286@gmail.com>
2020-01-24 12:13:37 -0800

    fix bug fo logging in with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,21 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable,  authentication_keys: [:login]
 
-  attr_writer :login
 
   def login
     @login || self.username || self.email
   end
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    conditions[:email].downcase! if conditions[:email]
-    where(conditions.to_h).first
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
+  def self.find_for_database_authentication warden_condition
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(
+      ["lower(username) = :value OR lower(email) = :value",
+        { value: login.strip.downcase}]).first
   end
 
   def validate_username
@@ -27,5 +24,5 @@ class User < ApplicationRecord
     end
   end
 
-  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+      validates :username, presence: :true, uniqueness: { case_sensitive: false }
 end
