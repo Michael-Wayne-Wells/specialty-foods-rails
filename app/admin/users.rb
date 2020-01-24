09971ede1bdf4e036bdba983bc73a1f00ae23891
Michael Wells <mwells1286@gmail.com>
2020-01-24 15:41:32 -0800

    add users admin console

---
+++ app/admin/users.rb
@@ -0,0 +1,14 @@
+ActiveAdmin.register User do
+  permit_params :email, :username, :password, :password_confirmation
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :username
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+end
