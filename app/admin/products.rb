23de55463d2c3fba85ee66925086d624a1fbe6da
Michael Wells <mwells1286@gmail.com>
2020-01-24 15:41:43 -0800

    add products admin console

---
+++ app/admin/products.rb
@@ -0,0 +1,12 @@
+ActiveAdmin.register Product do
+  permit_params :name, :cost, :country_of_origin
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :cost
+    column :country_of_origin
+    actions
+  end
+end
