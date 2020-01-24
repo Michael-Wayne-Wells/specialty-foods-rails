6af0568e2096ce3898e2189f2a2b3ebe4e430a08
Michael Wells <mwells1286@gmail.com>
2020-01-24 12:13:13 -0800

    add seeds for users, linking users to reviews, and making a default admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,20 @@
 Product.destroy_all
 Review.destroy_all
+User.destroy_all
+Admin.destroy_all
+
+Admin.create(email: "placeholder@email.com", username: "root", password: "ChangeMe")
 
 50.times do |index|
+  user = User.create!(email: Faker::Internet.email, username: Faker::Internet.username, password: Faker::Internet.password(min_length: 8))
   product = Product.create!(name: Faker::Food.ingredient,
     cost: Faker::Number.decimal(l_digits: 2, r_digits: 2) ,
     country_of_origin: Faker::Address.country)
     5.times do |j|
-      product.reviews.create!(author: Faker::Movies::StarWars.character, content_body: Faker::Lorem.paragraph(20, false, 0), rating: Faker::Number.within(range: 1..5))
+      product.reviews.create!(author: user.username, content_body: Faker::Lorem.paragraph(20, false, 0), rating: Faker::Number.within(range: 1..5))
     end
   end
 
   p "Created #{Product.count} products"
   p "Created #{Review.count} reviews"
+    p "Created #{User.count} reviews"
