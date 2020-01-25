e91afff15564b0c2b129b6f45463209bb5554777
Michael Wells <mwells1286@gmail.com>
2020-01-24 20:42:52 -0800

    add test for further integration

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
+
+
+
 describe "the add,delete, and edit a product process" do
+  Admin.delete_all
+  User.delete_all
+  Product.delete_all
+  Admin.create!(username: 'admin', email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+  Product.create(name:"test", cost: 22.22, country_of_origin: "usa")
+  User.create!(username: 'user', email: 'user@example.com', password: 'password', password_confirmation: 'password')
+  before(:each) do
+    visit new_admin_session_path
+    fill_in 'admin[login]', :with => "admin"
+    fill_in 'admin[password]', :with => "password"
+    click_on 'Login'
+  end
+
   it "adds a new product" do
+
     visit products_path
     click_link 'New Product'
     fill_in 'product[name]', :with => 'teeth'
@@ -48,11 +65,28 @@ describe "the add,delete, and edit a product process" do
     click_on 'Submit'
     click_on 'Teeth'
     click_on 'Add a review'
-    fill_in 'review[author]', :with => 'Jim'
     fill_in 'review[content_body]', :with => 'wow'
     fill_in 'review[rating]', :with => '3'
     click_on 'Submit'
-    expect(page).to have_content "Jim"
+    expect(page).to have_content "admin"
+  end
+  it "varifies user creation from admin" do
+    visit admin_users_path
+    expect(page).to have_content "user"
+    expect(page).to have_content "user@example.com"
+  end
+  it "varifies admin creation from admin" do
+    visit admin_admins_path
+    expect(page).to have_content "admin"
+    expect(page).to have_content "admin@example.com"
+  end
+  it "varifies product listing from admin console" do
+    visit admin_products_path
+    expect(page).to have_content "Test"
+    expect(page).to have_content "22.22"
+    expect(page).to have_content "Usa"
+
+
   end
 
 end
